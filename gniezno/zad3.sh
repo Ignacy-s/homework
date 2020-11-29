846ee6ed35cc799674d425e4a71f04f508cdbb7a
Ignacy Sawa <bd1cb5c6@opayq.com>
2020-11-29 18:29:17 +0100

    Trzecie zadanie.

--- gniezno/zad3.sh
+++ gniezno/zad3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Sun 29 Nov 2020 06:28:53 PM CET
+
 # Wyświetl zawartość plików podanych jako kolejne parametry wywołania
 # pod warunkiem że pliki te istnieją. Jeśli nie został podany żaden
 # plik należy wyświetlić komunikat: „Nie ma czego wyświetlać”. Treści
@@ -10,7 +12,16 @@ if [[ $# -eq 0 ]]; then
   echo "Nie ma czego wyświetlać."
 fi
 
+# Zmienna z gwiazdkam.
+gwiazdki="********************************************************************************"
 # Pętla tak długo jak są elementy.
+until [[ $# -eq 0 ]]; do
   # Sprawdzamy czy plik istnieje.
+  if [[ -a "$1" ]]; then
     # Jeśli plik istnieje, to go wypisujemy.
+    cat "$1"
     # Oddzielamy treść pliku gwiazdkami.
+    echo "$gwiazdki"
+  fi
+  shift
+done
