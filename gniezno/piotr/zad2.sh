b5f5068742d95cdfb3d63001c6c3d81e46d1dc7c
Ignacy Sawa <bd1cb5c6@opayq.com>
2020-11-29 17:24:49 +0100

    Drugie zadanie w wersji do oddania.

---
+++ gniezno/piotr/zad2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Napisz skrypt wyświetlający w kolejnych liniach argumenty przekazane
+# do skryptu. Skrypt napisać w trzech wariantach: pierwszy z
+# wykorzystaniem pętli for, drugi – pętli while i trzeci – petli
+# until.
+
+
+# Wariant `for`.
+
+# Petla for wykonana na wszystkich argumentach skryptu.
+for i in "$@" ; do
+  # "$@" wypisuje wszystkie argumenty obok siebie w cudzyslowach.
+  echo "$i"
+done
+
+# Petla while tak dlugo jak pozostaly jakies argumenty.
+while [[ $# -gt 0 ]]; do
+  echo "$1"
+  shift
+done
+
+# Petla until dopoki liczba argumentow nie jest rowna 0.
+until [[ $# -eq 0 ]]; do
+  echo "$1"
+  shift
+done
